f9ca59fb412db239a368052580974f0888a97c2a
Matt Smith <Matt@yuluthu.co.uk>
2026-04-09 23:55:52 +0100

    quick test that the /API route works before I stop for today

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function (Request $request) {
+    return response([
+        'success' => true,
+        'message' => 'Hello World',
+    ], 200);
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
